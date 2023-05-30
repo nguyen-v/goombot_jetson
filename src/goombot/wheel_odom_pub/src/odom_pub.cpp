9d45e3d6d817e9b16d347a672050c2113337c061
Vincent Nguyen <thanhvinhvincent.nguyen@epfl.ch>
2023-05-30 14:55:20 +0200

    working navigation with filtered scans

--- src/goombot/wheel_odom_pub/src/odom_pub.cpp
+++ src/goombot/wheel_odom_pub/src/odom_pub.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/String.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Pose2D.h>
@@ -17,13 +18,16 @@ using namespace std;
 #define REDUCTION_FACTOR      (60.f)
 #define TICKS_PER_REVOLUTION  (NUMBER_OF_POLES*REDUCTION_FACTOR)
 #define WHEEL_DIAMETER        (0.12f)
-#define WHEEL_SPACING         (0.332f)
-#define METERS_PER_TICKS      (M_PI*WHEEL_DIAMETER/TICKS_PER_REVOLUTION)
+const double WHEEL_SPACING =        (0.324f);
+// #define METERS_PER_TICKS      (M_PI*WHEEL_DIAMETER/TICKS_PER_REVOLUTION)
+const double METERS_PER_TICKS =       (1/640.f);
+const double TICKS_PER_METERS =     (640.f);
 
 // Publishers
 
 ros::Publisher odom_pub; // quaternions
 ros::Publisher odom_euler_pub; // euler angles (for visualisation)
+ros::Publisher calibrate_speed_pub; // speed calibration publisher
 
 // Odometry messages (x, y, yaw)
 
@@ -41,6 +45,12 @@ double velocity_right = 0;
 int dleft_ticks = 0;
 int dright_ticks = 0;
 
+int left_ticks = 0;
+int right_ticks = 0;
+
+int left_ticks_prev = 0;
+int right_ticks_prev = 0;
+
 // Only start publishing odometry when initial pose is received
 
 bool received_init_pose = false;
@@ -48,6 +58,11 @@ bool received_init_pose = false;
 // Handles
 
 void handle_init_pose(const geometry_msgs::Pose2D &init_pose) {
+  // Send calibration message to Arduino
+  std_msgs::String calibrate_speed_msg;
+  calibrate_speed_msg.data = "speed";
+  calibrate_speed_pub.publish(calibrate_speed_msg);
+  
   odom_prev.pose.pose.position.x = init_pose.x;
   odom_prev.pose.pose.position.y = init_pose.y;
   odom_prev.pose.pose.orientation.z = init_pose.theta;
@@ -59,9 +74,8 @@ void handle_init_pose(const geometry_msgs::Pose2D &init_pose) {
 // We assume that for both wheels, an increasing tick count means the robot is
 // going forward
 
-void compute_left_ticks(const std_msgs::Int16 &left_ticks) {
-  static int left_ticks_prev = 0;
-  dleft_ticks = left_ticks.data - left_ticks_prev;
+void compute_left_ticks() {
+  dleft_ticks = left_ticks - left_ticks_prev;
   
   if (dleft_ticks > 32767) { // "underflow"
     dleft_ticks -= 65535;
@@ -70,35 +84,82 @@ void compute_left_ticks(const std_msgs::Int16 &left_ticks) {
     dleft_ticks += 65535;
     // dleft_ticks = 65535 - dleft_ticks;
   }
-  distance_left = dleft_ticks*METERS_PER_TICKS;
-  left_ticks_prev = left_ticks.data;
+  // distance_left = dleft_ticks*METERS_PER_TICKS;
+  distance_left = dleft_ticks/TICKS_PER_METERS;
+  left_ticks_prev = left_ticks;
 }
 
-void compute_right_ticks(const std_msgs::Int16 &right_ticks) {
-  static int right_ticks_prev = 0;
-  dright_ticks = right_ticks.data - right_ticks_prev;
+void compute_right_ticks() {
+  dright_ticks = right_ticks - right_ticks_prev;
   
   if (dright_ticks > 32767) { // "underflow"
     dright_ticks -= 65535;
   }
-  else if (dleft_ticks < -32768) { // "overflow"
+  else if (dright_ticks < -32768) { // "overflow"
     dright_ticks += 65535;
     // dright_ticks = 65535 - dright_ticks;
   }
-  distance_right = dright_ticks*METERS_PER_TICKS;
-  right_ticks_prev = right_ticks.data;
+  // distance_right = dright_ticks*METERS_PER_TICKS;
+  distance_right = dright_ticks/TICKS_PER_METERS;
+  right_ticks_prev = right_ticks;
 }
+
+ void update_left_ticks(const std_msgs::Int16 &left_ticks_received) {
+  if (!received_init_pose) {
+    left_ticks_prev = left_ticks;
+  }
+  left_ticks = left_ticks_received.data;
+ }
+
+ void update_right_ticks(const std_msgs::Int16 &right_ticks_received) {
+  if (!received_init_pose) {
+    right_ticks_prev = right_ticks;
+  }
+  right_ticks = right_ticks_received.data;
+ }
+
+// void compute_left_ticks(const std_msgs::Int16 &left_ticks) {
+//   static int left_ticks_prev = 0;
+//   dleft_ticks = left_ticks.data - left_ticks_prev;
+  
+//   if (dleft_ticks > 32767) { // "underflow"
+//     dleft_ticks -= 65535;
+//   }
+//   else if (dleft_ticks < -32768) { // "overflow"
+//     dleft_ticks += 65535;
+//     // dleft_ticks = 65535 - dleft_ticks;
+//   }
+//   // distance_left = dleft_ticks*METERS_PER_TICKS;
+//   distance_left = dleft_ticks/TICKS_PER_METERS;
+//   left_ticks_prev = left_ticks.data;
+// }
+
+// void compute_right_ticks(const std_msgs::Int16 &right_ticks) {
+//   static int right_ticks_prev = 0;
+//   dright_ticks = right_ticks.data - right_ticks_prev;
+  
+//   if (dright_ticks > 32767) { // "underflow"
+//     dright_ticks -= 65535;
+//   }
+//   else if (dleft_ticks < -32768) { // "overflow"
+//     dright_ticks += 65535;
+//     // dright_ticks = 65535 - dright_ticks;
+//   }
+//   // distance_right = dright_ticks*METERS_PER_TICKS;
+//   distance_right = dright_ticks/TICKS_PER_METERS;
+//   right_ticks_prev = right_ticks.data;
+// }
 
 // we use the ticks to determine if velocity is zero
 
 void compute_left_speed(const std_msgs::Float32 &left_vel) {
-  // velocity_left = (dleft_ticks == 0 ? 0.0 : left_vel.data);
-  velocity_left = left_vel.data;
+  velocity_left = (dleft_ticks == 0 ? 0.0 : left_vel.data);
+  // velocity_left = left_vel.data;
 }
 
 void compute_right_speed(const std_msgs::Float32 &right_vel) {
-  // velocity_right = (dright_ticks == 0 ? 0.0 : right_vel.data);
-  velocity_right = right_vel.data;
+  velocity_right = (dright_ticks == 0 ? 0.0 : right_vel.data);
+  // velocity_right = right_vel.data;
 }
 
 // Wrap angle around [-PI, PI]
@@ -110,6 +171,8 @@ void wrap_angle(double &angle) {
 }
 
 void odom_update() {
+  compute_left_ticks();
+  compute_right_ticks();
   // https://medium.com/@nahmed3536/wheel-odometry-model-for-differential-drive-robotics-91b85a012299
   double avg_distance_travelled = (distance_left + distance_right)/2;
   double angle_turned = (distance_right - distance_left)/WHEEL_SPACING;
@@ -205,9 +268,9 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
 
   // Setup subscribers
-  ros::Subscriber left_ticks_sub = nh.subscribe("left_ticks", 100, compute_left_ticks, 
+  ros::Subscriber left_ticks_sub = nh.subscribe("left_ticks", 100, update_left_ticks, 
                                                 ros::TransportHints().tcpNoDelay());
-  ros::Subscriber right_ticks_sub = nh.subscribe("right_ticks", 100, compute_right_ticks, 
+  ros::Subscriber right_ticks_sub = nh.subscribe("right_ticks", 100, update_right_ticks, 
                                                 ros::TransportHints().tcpNoDelay());  
 
   ros::Subscriber left_vel_sub = nh.subscribe("left_wheel_vel", 100, compute_left_speed, 
@@ -220,7 +283,7 @@ int main(int argc, char **argv) {
   // Setup publishers
   odom_pub = nh.advertise<nav_msgs::Odometry>("odom_quat", 100);
   odom_euler_pub = nh.advertise<nav_msgs::Odometry>("odom_euler", 100);
-
+  calibrate_speed_pub = nh.advertise<std_msgs::String>("reset_controller", 10);
 
   // We assume the robot is at standstill intially
   // with no roll or pitch
